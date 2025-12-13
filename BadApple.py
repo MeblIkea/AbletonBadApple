ea0418c687b7590c34e9a008ee81395303f6bd14
MeblIkea <69876322+MeblIkea@users.noreply.github.com>
2025-12-13 15:29:26 +0100

    Final result

--- BadApple.py
+++ BadApple.py
@@ -1,52 +1,60 @@
 from __future__ import with_statement
-
-import random
+from .badapple_frames import FRAMES
 import time
 
 import Live
 import threading
+
 from _Framework.ControlSurface import ControlSurface
 
+FPS = 15
+W = 64
+H = 36
+
+
 def messing(tracks):
-    while True:
-        for clips in tracks:
-            for clip in clips:
+    start_time = time.time()
+    for i in range(len(FRAMES)):
+        frame = FRAMES[i]
+        start_time += 1 / FPS
+        for x, clips in enumerate(tracks):
+            for y, clip in enumerate(clips):
                 try:
-                    clip.color_index = random.randint(20, 50)
+                    if frame[x + y * W] != clip.color_index:
+                        clip.color_index = frame[x + y * W]
                 except Exception:
                     pass
-        time.sleep(0.01)
-    Live.Base.log(f"Threading! {colors}")
+        if time.time() < start_time:
+            time.sleep(1 / FPS)
+    Live.Base.log(f"Threading!")
+
 
 class BadApple(ControlSurface):
     def __init__(self, c_instance):
         ControlSurface.__init__(self, c_instance)
-        Live.Base.log("Challenge ready!")
-        self.checkIfReady()
+
+        self.frames = []
+        Live.Base.log(f"Challenge ready with {len(FRAMES)} frames")
+        self.tracks = []
         song = Live.Application.get_application().get_document()
-        clips = []
-        w = 16
-        h = 9
-        for _ in range(h-8):
+
+        self.setup_set()
+        t = threading.Thread(target=messing, args=(self.tracks,))
+        song.add_loop_listener(lambda: t.start())
+
+    def setup_set(self):
+        song = Live.Application.get_application().get_document()
+        for _ in range(H - 8):
             song.create_scene(0)
-        for i in range(w):
+        for i in range(W):
             track = song.create_midi_track(0)
             clip_list = []
-            clips.append(clip_list)
+            self.tracks.append(clip_list)
             for clip_slot in track.clip_slots:
                 clip_slot.create_clip(1)
                 clip_slot.clip.color_index = 69
                 clip_list.append(clip_slot.clip)
-                Live.Base.log(f"Challenge ready! {clip_slot.clip._live_ptr}")
 
+        self.tracks.reverse()
         for _ in range(4):
-            song.delete_track(w)
-        threading.Thread(target=messing, args=(clips,)).start()
-
-        #for ret in song.return_tracks:
-        #    song.delete_return_track(ret)
-
-
-    @staticmethod
-    def checkIfReady():
-        Live.Base.log(Live.Application.get_application().get_document().master_track.name)
+            song.delete_track(W)
---
+++ frames/_frame_parser.py
@@ -0,0 +1,49 @@
+﻿import glob
+import os
+
+from PIL import Image
+import cv2
+
+W = 64
+H = 36
+if __name__ == '__main__':
+    cap = cv2.VideoCapture('_badapple.mp4')
+    if True:
+        print("Video > Images")
+        if not os.path.isdir("frames"):
+            os.mkdir("frames")
+        i = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if frame is None:
+                print("done")
+                break
+            if i % 2 == 0:
+                i += 1
+                continue
+            frame = cv2.resize(frame, (W, H), fx=0, fy=0, interpolation=cv2.INTER_AREA)
+            cv2.imwrite(f"frames/{str(i).zfill(5)}.png", frame)
+            i += 1
+            # define q as the exit button
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        # release the video capture object
+        cap.release()
+        # Closes all the windows currently opened.
+        cv2.destroyAllWindows()
+
+    print("Images > Array")
+    frames = []
+    for path in sorted(glob.glob("frames/*.png")):
+        img = Image.open(path)
+        pixels = list(img.getdata(0))
+        frame = [13 if p > 128 else 69 for p in pixels]
+        frames.append(frame)
+
+    print("Array > Python")
+    with open("../badapple_frames.py", "w") as f:
+        f.write("FRAMES = ")
+        f.write(repr(frames))
+
+    print(f"Parsed {len(frames)} frames")
